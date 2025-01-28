91030747371a7f3f722d81aa1b0991bd787911ad
Jason Wang <Jason.wang2@gordon.edu>
2025-01-28 11:02:18 -0500

    added diagonal test

--- tictactoe_test.cc
+++ tictactoe_test.cc
@@ -1,7 +1,7 @@
 // Tests for tictactoe.cc
 //
 // Author (starting point): Russ Tuck
-// Authors (everything else):
+// Authors (everything else): Jason Wang
 
 #include <iostream>
 using std::cout;
@@ -25,12 +25,28 @@ bool tttb_winner_vertical_test() {
     return true;
 }
 
+bool ttb_winnder_diagonal_test(){
+    TictactoeBoard b;
+    b.set(0, 0, TictactoeXO::X);
+    b.set(1, 1, TictactoeXO::X);
+    if (TictactoeXO::X == b.winner()) {
+        cout << "tttb_winner_diagonal_test() failed for 2 in a row" << endl;
+        return false;
+    }
+    b.set(2, 2, TictactoeXO::X);
+    if (TictactoeXO::X != b.winner()) {
+        cout << "tttb_winner_diagonal_test() failed for 3 in a row" << endl;
+        return false;
+    }
+    return true;
+}
 
 // Runs tests above, and prints a message if they all pass.
 // If any fail, they print their own failure messages.
 int main() {
     bool result = true;
     result &= tttb_winner_vertical_test();
+    result &= ttb_winnder_diagonal_test();
     if (result) {
         cout << "All tests passed." << endl;
         return(0);
